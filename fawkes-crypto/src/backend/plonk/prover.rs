054308b78d4612288b5ac7c300a4a38051cc9de3
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2023-02-13 19:15:38 +0100

    Temporary commit: plonk backend wip

--- fawkes-crypto/src/backend/plonk/prover.rs
+++ fawkes-crypto/src/backend/plonk/prover.rs
@@ -18,7 +18,17 @@ use crate::{
 
 /// This runs a `MockProver` on a `BuildCS` value. Returns `Ok(true)` if
 /// circuit was built and verified correctly.
-pub fn mock_prove<F: Field + PrimeField + Ord>(k: u32, cs: &BuildCS<F>) -> bool {
+pub fn mock_prove<F: Field + PrimeField + Ord>(cs: &BuildCS<F>) -> bool {
+    use std::cmp::max;
+
+    // Maximum number of halo2 rows. It limits the allowed number of gates and
+    // inputs for our circuit. Shouldn't be greater than 2^18.
+    //
+    // TODO: We may need to increase this value a bit, since halo2's Layouter
+    // may not fit our values perfectly, or may use a couple of rows for its
+    // own stuff.
+    let k = max(cs.gates.len(), cs.public.len()) as u32;
+
     let inputs = extract_inputs(cs).into_iter().map(|i| i.unwrap().0).collect();
     let prover = MockProver::run(k, cs, vec![inputs]).unwrap();
     prover.verify().is_ok()
