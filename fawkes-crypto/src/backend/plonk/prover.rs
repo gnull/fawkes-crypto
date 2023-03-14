33fd16de3ee58afc6326498ab1b7166c68191e33
Ivan Oleynikov <ivan.oleynikov95@gmail.com>
2023-03-14 17:09:51 +0100

    implement mock prover for halo2 backend

--- fawkes-crypto/src/backend/plonk/prover.rs
+++ fawkes-crypto/src/backend/plonk/prover.rs
@@ -2,35 +2,38 @@ use super::halo2_circuit::*;
 
 // use group::{ff::Field, prime::PrimeCurve};
 use halo2_proofs::{
-    arithmetic::{Field, Group},
-    circuit::{AssignedCell, Chip, Layouter, Region, SimpleFloorPlanner, Value},
-    plonk::{Advice, Any, Circuit, Column, ConstraintSystem, Error, Fixed, Instance, Selector},
-    poly::Rotation, dev::MockProver, // dev::metadata::Column,
+    dev::MockProver,
+    plonk::create_proof,
+    halo2curves::FieldExt,
 };
 
 use crate::{
   circuit::{
     cs::*,
-    num::*,
-    bool::*,
   },
-  ff_uint::{Num, PrimeField},
+  ff_uint::{PrimeField},
 };
 
-// /// This runs a `MockProver` on a `BuildCS` value. Returns `true` if circuit
-// /// was built and verified correctly.
-// pub fn mock_prove<F: Group + Field + PrimeField + Ord>(cs: &BuildCS<F>) -> bool {
-//     use std::cmp::max;
+use super::{
+    fawkes_cs_to_halo,
+};
 
-//     // Maximum number of halo2 rows. It limits the allowed number of gates and
-//     // inputs for our circuit. Shouldn't be greater than 2^18.
-//     //
-//     // TODO: We may need to increase this value a bit, since halo2's Layouter
-//     // may not fit our values perfectly, or may use a couple of rows for its
-//     // own stuff.
-//     let k = max(cs.gates.len(), cs.public.len()) as u32;
+/// This runs a `MockProver` on a `BuildCS` value. Returns `true` if circuit
+/// was built and verified correctly. The `Fy` type parameter specifies the
+/// field type that the numbers in `BuildCS<Fx>` should be converted to.
+pub fn mock_prove<Fx: PrimeField, Fy: FieldExt>(cs: BuildCS<Fx>) -> bool {
+    use std::cmp::max;
 
-//     let inputs = extract_inputs(cs).into_iter().map(|i| i.unwrap().0).collect();
-//     let prover = MockProver::run(k, cs, vec![inputs]).unwrap();
-//     prover.verify().is_ok()
-// }
+    // Maximum number of halo2 rows. It limits the allowed number of gates and
+    // inputs for our circuit. Shouldn't be greater than 2^18.
+    //
+    // TODO: We may need to increase this value a bit, since halo2's Layouter
+    // may not fit our values perfectly, or may use a couple of rows for its
+    // own stuff.
+    let k = max(cs.gates.len(), cs.public.len()) as u32;
+
+    let (cs, ins) = fawkes_cs_to_halo::<Fx, Fy>(cs);
+    let ins = ins.into_iter().map(|i| i.unwrap()).collect();
+    let prover = MockProver::run(k, &cs, vec![ins]).unwrap();
+    prover.verify().is_ok()
+}
